521f77c67432e16ed50b11ae90e9caabfd0225eb
Victor Paranhos <victor_paranhos@hotmail.com>
2022-04-05 13:02:26 -0300

    Atulizando 2

--- funcoes.js
+++ funcoes.js
@@ -1,7 +1,10 @@
+
 const MenuSite = document.querySelectorAll('.section-1')
 
 
 function openClose() {
+  
+      /*Abrir e fechar menu SITES */
       for (let element of MenuSite) {
         element.classList.toggle('active')
       }
